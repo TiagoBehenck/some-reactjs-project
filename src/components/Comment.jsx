9b8fcd0df6890c5fab3b08a60fb7bc8938469cac
Tiago Behenck <tiago.behenck.santos@gmail.com>
2023-07-11 16:16:48 -0300

    style of comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -18,7 +18,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito pika!</p>
@@ -26,7 +26,7 @@ export function Comment() {
 
         <footer>
           <button>
-            <ThumbsUp size={20} />
+            <ThumbsUp />
             Aplaudir <span>20</span>
           </button>
         </footer>
